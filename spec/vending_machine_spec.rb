730e40ad72c9fe5f12a35b54920ca5e2e0323047
sue445 <sue445@sue445.net>
2012-09-01 13:57:15 +0900

    refactor

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -3,19 +3,18 @@ require "spec_helper"
 
 describe VendingMachine do
   describe '#total' do
+    let(:vm){ VendingMachine.new }
+
     it '何もしない状態で総計を取得すると0円が返ること' do
-      vm = VendingMachine.new
-      vm.total.should eq(0)
+      vm.total.should == 0
     end
 
     it '10円玉を入れた時に10円が返ること' do
-      vm = VendingMachine.new
       vm.insert(10)
       vm.total.should == 10
     end
 
     it '50円玉と100円玉を入れた時に150円が返ること' do
-      vm = VendingMachine.new
       vm.insert(50)
       vm.insert(100)
       vm.total.should == 150
